9095547c8434e557c728be359adaf0b329d6b0eb
calvarado2004 <carlos-alvarado@outlook.com>
2023-04-13 23:47:22 -0400

    using react-router-dom

---
+++ go-movies-front-end/src/components/ManageCatalog.js
@@ -0,0 +1,13 @@
+
+const ManageCatalog = () => {
+    return (
+        <>
+            <div className="text-center">
+                <h2>ManageCatalog</h2>
+                <hr/>
+            </div>
+        </>
+    )
+}
+
+export default ManageCatalog
--- go-movies-front-end/src/index.js
+++ go-movies-front-end/src/index.js
@@ -5,6 +5,11 @@ import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import ErrorPage from "./components/ErrorPage";
 import Home from "./components/home";
 import Movies from "./components/movies";
+import Genres from "./components/genres";
+import EditMovie from "./components/editMovie";
+import ManageCatalog from "./components/ManageCatalog";
+import Login from "./components/Login";
+import GraphQL from "./components/GraphQL";
 
 const router = createBrowserRouter([
     {
@@ -14,6 +19,14 @@ const router = createBrowserRouter([
         children: [
             {index: true, element:  <Home/>},
             {path: '/movies', element: <Movies/>},
+            {path: '/genres', element: <Genres/>},
+            {path: '/admin/movie/0', element: <EditMovie/>},
+            {path: '/manage-catalog', element: <ManageCatalog/>},
+            {path: '/graphql', element: <GraphQL/>},
+            {path: '/login', element: <Login/>},
+
+
+
         ]
     },
 
