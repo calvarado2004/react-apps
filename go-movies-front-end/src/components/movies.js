af26067f8a7794e97bb9f485e5ad4f7c6feb696d
calvarado2004 <carlos-alvarado@outlook.com>
2023-04-16 23:22:44 -0400

    adding connectivity to Golang Backend

--- go-movies-front-end/src/components/movies.js
+++ go-movies-front-end/src/components/movies.js
@@ -5,27 +5,24 @@ const Movies = () => {
     const [movies, setMovies] = react.useState([])
 
     useEffect(() => {
-        let moviesList = [
-            {
-                id: 1,
-                title: "The Shawshank Redemption",
-                release_date: "1994-09-23",
-                runtime: 142,
-                mpaa_rating: "R",
-                description: "Synopsis. In 1947, Andy Dufresne (Tim Robbins), a banker in Maine, is convicted of murdering his wife and her lover, a golf pro. Since the state of Maine has no death penalty, he is given two consecutive life sentences and sent to the notoriously harsh Shawshank Prison.\n",
 
-            },
-            {
-                id: 2,
-                title: "Titanic",
-                release_date: "1997-11-18",
-                runtime: 194,
-                mpaa_rating: "PG-13",
-                description: "The movie is about the 1912 sinking of the RMS Titanic. It stars Kate Winslet and Leonardo DiCaprio. The two play characters who are of different social classes. They fall in love after meeting aboard the ship, but it was not good for a rich girl to fall in love with a poor boy in 1912.\n",
+        const headers = new Headers();
+        headers.append("Content-Type", "application/json");
 
-            },
-        ];
-        setMovies(moviesList);
+        const requestOptions = {
+            method: "GET",
+            headers: headers,
+            redirect: "follow",
+
+        };
+
+        fetch(`http://localhost:8080/movies`, requestOptions)
+            .then(response => response.json())
+            .then((result) => {
+                setMovies(result);
+            })
+            .catch((error) => console.log("error", error));
+
 
     }, []);
 
