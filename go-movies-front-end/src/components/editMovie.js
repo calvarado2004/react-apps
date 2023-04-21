a26bce0279408d9f1cabe0a34dcc3c5879d1ad8a
calvarado2004 <carlos-alvarado@outlook.com>
2023-04-21 17:51:12 -0400

    adding edit movie component

--- go-movies-front-end/src/components/editMovie.js
+++ go-movies-front-end/src/components/editMovie.js
@@ -1,10 +1,70 @@
+import {useNavigate, useOutletContext, useParams} from "react-router-dom";
+import {useEffect, useState} from "react";
+import Input from "./form/input";
 
 const EditMovie = () => {
+
+    const navigate = useNavigate();
+    const { jwtToken } = useOutletContext()
+    const [error, setError] = useState(null);
+    const [errors, setErrors] = useState([]);
+
+    const hasError = (key) => {
+        return errors.indexOf(key) !== -1;
+    }
+
+    const [movie, setMovie] = useState({
+        id: 0,
+        title: "",
+        release_date: "",
+        runtime: 0,
+        mpaa_rating: "",
+        description: "",
+    });
+
+    // get id from url
+    let {id} = useParams();
+
+    useEffect(() => {
+        if (jwtToken === "") {
+            navigate("/login")
+            return;
+        }
+    }, [jwtToken, navigate]);
+
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    }
+
+    const handleChange = () => (event) => {
+        let value = event.target.value;
+        let name = event.target.name;
+        setMovie({...movie, [name]: value})
+    };
+
+
     return (
         <>
             <div className="text-center">
-                <h2>EditMovie</h2>
+                <h2>Add/Edit Movie</h2>
                 <hr/>
+                <pre>{JSON.stringify(movie, null, 3)}</pre>
+
+                <form onSubmit={handleSubmit}>
+                    <input type="hidden" name="id" value={movie.id} id="id"/>
+                    <Input
+                        title={"Title"}
+                        className={"form-control"}
+                        type={"text"}
+                        name={"title"}
+                        value={movie.title}
+                        onChange={handleChange("title")}
+                        errorDiv={hasError("title") ? "text-danger" : "d-none"}
+                        errorMsg={"Please enter a title"}
+                    />
+
+                </form>
             </div>
         </>
     )
