4c99e25ac1ab2de042aaea7f49c5d828c3dd1222
calvarado2004 <carlos-alvarado@outlook.com>
2023-04-24 22:52:51 -0400

    genres on frontend

--- go-movies-front-end/src/components/genres.js
+++ go-movies-front-end/src/components/genres.js
@@ -1,13 +1,70 @@
+import {useEffect, useState} from "react";
+import {Link} from "react-router-dom";
 
 const Genres = () => {
-    return (
-        <>
-            <div className="text-center">
+
+
+
+    const [genres, setGenres] = useState([]);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+
+        const requestOptions = {
+            method: 'GET',
+            headers: headers,
+        };
+
+        fetch('/genres', requestOptions)
+            .then((response) => response.json())
+            .then(data => {
+                if (data.error) {
+                    setError(data.message);
+                } else {
+                    setGenres(data);
+                }
+            })
+            .catch((error) => {
+                setError(error);
+            })
+    }, []);
+
+    if (error !== null) {
+        return (
+            <>
+                <div className="text-center">
+                    <h2>Genres</h2>
+                    <hr/>
+                    <div className="alert alert-danger">
+                        {error}
+                    </div>
+                </div>
+            </>
+        )
+    } else {
+        return (
+            <div>
                 <h2>Genres</h2>
                 <hr/>
+                <div className="list-group">
+                    {genres?.map((g) => (
+                        <Link key={g.id}
+                              className="list-group-item list-group-item-action"
+                              to={`/genres/${g.id}`}
+                              state={
+                                  {
+                                      genreName: g.genre,
+                                  }
+                              }>{g.genre}</Link>
+                    ))}
+                </div>
             </div>
-        </>
-    )
+        )
+
+    }
+
 }
 
 export default Genres
